7b1fcfd340174fe431d2f1825958d47441bb9b17
Teguh Hidayatullah <thidayatullah90@gmail.com>
2024-05-28 15:50:51 +0700

    Fixed Message.schedule wrong date

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import prisma from "./prismaClient";
 import express, { Request, Response } from "express";
 import createError from "http-errors";
-import { scheduleDailyMessages, scheduleHourlyRetry } from "./scheduler";
+import {
+  scheduleDailyMessages,
+  scheduleHourlyRetry,
+  scheduleBirthdayMessages,
+  deleteOldSchedule,
+} from "./scheduler";
 
 const app = express();
 
@@ -19,21 +24,14 @@ app.post("/user", async (req: Request, res: Response) => {
     });
 
     const today = new Date();
-    const thisYearBirthday = new Date(
-      today.getFullYear(),
-      birthdayDate.getMonth(),
-      birthdayDate.getDate()
-    );
-    const nextBirthday = new Date(
-      today.getFullYear() + 1,
-      birthdayDate.getMonth(),
-      birthdayDate.getDate()
-    );
-    let isBirthdayPassed = thisYearBirthday <= today;
-    console.log(thisYearBirthday);
-    console.log(today);
-    console.log(thisYearBirthday > today);
-    console.log(thisYearBirthday < today);
+
+    const thisYearBirthday = new Date(birthday);
+    thisYearBirthday.setFullYear(today.getFullYear());
+
+    const nextBirthday = new Date(birthday);
+    nextBirthday.setFullYear(today.getFullYear() + 1);
+
+    const isBirthdayPassed = thisYearBirthday <= today;
 
     await prisma.message.create({
       data: {
@@ -58,6 +56,7 @@ app.put("/user/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   const { birthday } = req.body;
   const birthdayDate = new Date(birthday);
+
   try {
     const user = await prisma.user.update({
       where: { id: parseInt(id) },
@@ -68,17 +67,14 @@ app.put("/user/:id", async (req: Request, res: Response) => {
     });
 
     const today = new Date();
-    const thisYearBirthday = new Date(
-      today.getFullYear(),
-      birthdayDate.getMonth(),
-      birthdayDate.getDate()
-    );
-    const nextBirthday = new Date(
-      today.getFullYear() + 1,
-      birthdayDate.getMonth(),
-      birthdayDate.getDate()
-    );
-    let isBirthdayPassed = thisYearBirthday <= today;
+
+    const thisYearBirthday = new Date(birthday);
+    thisYearBirthday.setFullYear(today.getFullYear());
+
+    const nextBirthday = new Date(birthday);
+    nextBirthday.setFullYear(today.getFullYear() + 1);
+
+    const isBirthdayPassed = thisYearBirthday <= today;
 
     const message = await prisma.message.findFirst({
       where: { userID: user.id },
@@ -87,16 +83,20 @@ app.put("/user/:id", async (req: Request, res: Response) => {
       message &&
       new Date(birthday).toISOString() !== message.schedule.toISOString()
     ) {
-      await prisma.message.update({
+      await deleteOldSchedule(`${user.id}-birthday`);
+
+      const updatedMessage = await prisma.message.update({
         where: { id: message.id },
         data: { schedule: isBirthdayPassed ? nextBirthday : thisYearBirthday },
       });
+
+      scheduleBirthdayMessages(updatedMessage, user);
     }
 
     res.status(200).send(user);
   } catch (error: unknown) {
     if (error instanceof Error) {
-      res.status(404).send({ error: error.message });
+      res.status(404).send({ error: "User not found" });
     } else {
       res.status(400).send({ error: "An unknown error occurred" });
     }
@@ -106,6 +106,11 @@ app.put("/user/:id", async (req: Request, res: Response) => {
 app.delete("/user/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
+    const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
+    console.log("user ni bos: ", user);
+    if (user) {
+      await deleteOldSchedule(`${user.id}-birthday`);
+    }
     await prisma.message.deleteMany({ where: { userID: parseInt(id) } });
     await prisma.user.delete({ where: { id: parseInt(id) } });
     res.status(204).send();
